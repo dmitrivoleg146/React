7ed670206cb29f0368ad00c9b3cc9c246a371bdb
Oleh <dmitrivoleg146@gmail.com>
2023-01-03 15:10:12 +0200

    React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Job from './Job';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Job salary={9000} title="Junior" company="Binance"/>
+       <Job salary={20000} title="Middle" company="Okex"/>
+       <Job salary={30000} title="Seniour" company="Mexc"/>
     </div>
   );
 }
