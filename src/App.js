27d2bb3259c8941911d7f813b03885a6d6c9b83e
Oleh <dmitrivoleg146@gmail.com>
2023-01-04 17:54:43 +0200

    Planets

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import styles from './App.module.css';
 import Job from './Job';
 import List from './List'
+import Planets from './Planets';
 
 function App() {
-  const age = 15;
-  const isGrean = true;
+  // const age = 15;
+  // const isGrean = true;
+  const planets =[
+    {name:"Mars" , isGasPlanet:true},
+    {name:"Earth", isGasPlanet:true},
+    {name:"Uranus", isGasPlanet:false},
+    {name:"Neptune", isGasPlanet:false}
+  ]
   
   return (
     <div className={styles.App}>
-      {age>=19 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
+      {planets.map((planet,key)=> !planet.isGasPlanet &&
+          <h1>{planet.name}</h1>
+      )}
+
+      {/* //with {} works if in map, but for ternary we don't use them and not use a return */}
+        {/* {age>=19 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
       <h1 style={{color: isGrean ? "green" : "red" }}></h1>
        <Job salary={9000} title="Junior" company="Binance"/>
        <Job salary={20000} title="Middle" company="Okex"/>
        <Job salary={30000} title="Seniour" company="Mexc"/>
        {isGrean ? <button>Click me</button> : <h1>Cool</h1>}
-       <List />
+       <List />  */}
     </div>
-  ); //40
+  ); 
 }
 
 export default App;
---
+++ src/Planets.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Planets = (props) => {
+  return (
+    <div>{props.name} {props.age}</div>
+  )
+}
+
+export default Planets
