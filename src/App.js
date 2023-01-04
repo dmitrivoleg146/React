9a02d6348ce409f3f93e1a51ead81184e4b80e8b
Oleh <dmitrivoleg146@gmail.com>
2023-01-04 17:08:14 +0200

    operators

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import './App.css';
+import styles from './App.module.css';
 import Job from './Job';
 
 function App() {
+  const age = 15;
+  const isGrean = true;
+  
   return (
-    <div className="App">
+    <div className={styles.App}>
+      {age>=19 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
+      <h1 style={{color: isGrean ? "green" : "red" }}></h1>
        <Job salary={9000} title="Junior" company="Binance"/>
        <Job salary={20000} title="Middle" company="Okex"/>
        <Job salary={30000} title="Seniour" company="Mexc"/>
+       {isGrean ? <button>Click me</button> : <h1>Cool</h1>}
     </div>
-  );
+  ); //40
 }
 
 export default App;
